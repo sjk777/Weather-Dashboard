ea62900c20a1d9e4ffeeea0457df46a3e8a2e443
Sky K <skyj.kim7@gmail.com>
2023-02-23 10:29:33 -0800

    added APIkey defined today using dayjs and added variables for the day after and search

--- assets/script.js
+++ assets/script.js
@@ -7,3 +7,11 @@ var windNow = document.querySelector('.wind');
 var humidNow = document. querySelector('.humidity');
 var searchCity = docuement.querySelector('.cityListContainer');
 var cityBtn = document.querySelector('.cityButton');
+
+var APIKey= '43973cffb25813812c8321c091ad172c';
+var today= dayjs().format("MM/DD/YYYY");
+var nextDay= "";
+
+
+var searchCities = [];
+
