fd7baa409d9f6100f6789646a1a4966fbe8f0241
Sky K <skyj.kim7@gmail.com>
2023-02-23 10:38:31 -0800

    added fetch request and element create buttons with innerHTML to append properties to index

--- assets/script.js
+++ assets/script.js
@@ -36,3 +36,20 @@ function saveToList(){
     }
 }
 
+function getApi(city){
+    var queryURL='https://api.openweathermap.org/geo/1.0/direct?q='+city+'&limit=1&appid='+APIKey
+    fetch(queryURL)
+        .then(function(response){
+            return response.json
+        })
+        .then(function (data){
+            var latitude = data[0].lat
+            var longitude = data[0].lon
+            getWeather(latitude,longitude)
+        });
+}
+
+function getWeather(latitude,longitude){
+    var weatherURL = 'https://api.openweathermap.org/data/2.5/forecast?lat='+latitude+'&lon='+longitude+'&cnt=6&units=imperial&appid='+ APIKey
+    
+}
