2ec27657b05296c3804315b3067028e62bee2769
Sky K <skyj.kim7@gmail.com>
2023-02-23 10:26:46 -0800

    added variables

---
+++ assets/script.js
@@ -0,0 +1,9 @@
+var button = document.querySelector('.btn');
+var inputCity= document.querySelector('#city');
+var dailyW = document.querySelector('.dailyW');
+var cityName= document.querySelector('.cityName');
+var tempNow = document.querySelector('.temp');
+var windNow = document.querySelector('.wind');
+var humidNow = document. querySelector('.humidity');
+var searchCity = docuement.querySelector('.cityListContainer');
+var cityBtn = document.querySelector('.cityButton');
